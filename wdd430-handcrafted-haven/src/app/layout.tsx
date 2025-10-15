33a11c3e0f3af32d250cf07bdb2b335641931219
Jigsawsam <bgt117@gmail.com>
2025-10-15 13:53:40 -0700

    up styles nav

--- wdd430-handcrafted-haven/src/app/layout.tsx
+++ wdd430-handcrafted-haven/src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Poppins, Inter, Source_Code_Pro } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./page.module.css";
+
 
 // === Fonts ===
 const poppins = Poppins({
@@ -41,43 +41,24 @@ export default function RootLayout({
         className={`${poppins.variable} ${inter.variable} ${sourceCodePro.variable}`}
       >
         {/* ===== Shared Icon Navigation ===== */}
-        <nav className={styles.iconNav}>
-          <Link href="/" className={styles.iconWrapper}>
-            <Image
-              src="/home-icon.png"
-              alt="Home"
-              fill
-              className={styles.navIcon}
-            />
-          </Link>
+        <nav className="iconNav">
+  <Link href="/" className="iconWrapper">
+    <Image src="/home-icon.png" alt="Home" fill className="navIcon" />
+  </Link>
+  <Link href="/shop" className="iconWrapper">
+    <Image src="/shop-icon.png" alt="Shop" fill className="navIcon" />
+  </Link>
+  <Link href="/cart" className="iconWrapper">
+    <Image src="/cart-icon.png" alt="Cart" fill className="navIcon" />
+  </Link>
+  <Link href="/sell" className="iconWrapper">
+    <Image src="/sell-icon.png" alt="Sell" fill className="navIcon" />
+  </Link>
+  <Link href="/login" className="iconWrapper">
+    <Image src="/login-icon.png" alt="Login" fill className="navIcon" />
+  </Link>
+</nav>
 
-          <Link href="/shop" className={styles.iconWrapper}>
-            <Image
-              src="/shop-icon.png"
-              alt="Shop"
-              fill
-              className={styles.navIcon}
-            />
-          </Link>
-
-          <Link href="/sell" className={styles.iconWrapper}>
-            <Image
-              src="/sell-icon.png"
-              alt="Sell"
-              fill
-              className={styles.navIcon}
-            />
-          </Link>
-
-          <Link href="/login" className={styles.iconWrapper}>
-            <Image
-              src="/login-icon.png"
-              alt="Login"
-              fill
-              className={styles.navIcon}
-            />
-          </Link>
-        </nav>
 
         {/* ===== Page Content ===== */}
         {children}
